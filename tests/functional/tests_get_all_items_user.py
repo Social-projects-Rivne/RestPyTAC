422e243f6fca1dc91be4d6b84c399146bf4ef375
Pavlo Bezrukov <bezrukov0205@gmail.com>
2018-10-29 18:37:27 +0200

    Some changes in the tests files

--- tests/functional/tests_get_all_items_user.py
+++ tests/functional/tests_get_all_items_user.py
@@ -1,5 +1,10 @@
 from tests.functional import ApiTestBase
-from tests.constants.constants import InitUsers, DefaultUser, VALID_STATUS_CODE
+from random import choice, randint
+from tests.constants.constants import InitUsers, DefaultUser, VALID_STATUS_CODE, ITEM_NAMES, INVALID_TOKEN
+
+
+item_index = randint(0, 1000)
+item_name = choice(ITEM_NAMES)
 
 
 class TestAllUserItems(ApiTestBase):
@@ -21,11 +26,44 @@ class TestAllUserItems(ApiTestBase):
                 self.assertEqual(VALID_STATUS_CODE, get_items_user_response.status_code)
                 self.assertFalse(get_items_user_response.json()["content"])
 
+    def test_get_items_by_admin_with_invalid_token(self):
+        """get items by admin with invalid token"""
+        self.login(DefaultUser.user, DefaultUser.password)
+        for user in InitUsers.users:
+            with self.subTest(i=user):
+                get_items_user_response = self.get_user_all_items(user, INVALID_TOKEN)
+                self.assertEqual(VALID_STATUS_CODE, get_items_user_response.status_code)
+                self.assertFalse(get_items_user_response.json()["content"])
+
+    def test_get_added_items_by_admin(self):
+        """get added user items by admin"""
+        for user, password in InitUsers.users.items():
+            token = self.login(user, password).json()["content"]
+            self.add_item(item_index, token, item_name)
+        admintoken = self.login(DefaultUser.user, DefaultUser.password).json()["content"]
+        for user in InitUsers.users:
+            with self.subTest(i=user):
+                get_items_user_response = self.get_user_all_items(user, admintoken)
+                self.assertEqual(VALID_STATUS_CODE, get_items_user_response.status_code)
+                self.assertTrue(get_items_user_response.json()["content"])
+
     def test_get_items_by_user(self):
         """get user items with user token"""
         for user in InitUsers.users:
             with self.subTest(i=user):
                 token = self.login("kilinatc", "password").json()["content"]
                 get_items_user_response = self.get_user_all_items(user, token)
+                self.assertEqual(VALID_STATUS_CODE, get_items_user_response.status_code)
+                self.assertFalse(get_items_user_response.json()["content"])
+
+    def test_get_added_items_by_user(self):
+        """get added user items by user"""
+        for user, password in InitUsers.users.items():
+            token = self.login(user, password).json()["content"]
+            self.add_item(item_index, token, item_name)
+        admintoken = self.login("akinatc", "password").json()["content"]
+        for user in InitUsers.users:
+            with self.subTest(i=user):
+                get_items_user_response = self.get_user_all_items(user, admintoken)
                 self.assertEqual(VALID_STATUS_CODE, get_items_user_response.status_code)
                 self.assertFalse(get_items_user_response.json()["content"])
--- tests/functional/tests_get_user_item_by_index.py
+++ tests/functional/tests_get_user_item_by_index.py
@@ -1,5 +1,9 @@
 from tests.functional import ApiTestBase
-from tests.constants.constants import InitUsers, DefaultUser, VALID_STATUS_CODE
+from random import randint
+from tests.constants.constants import InitUsers, DefaultUser, VALID_STATUS_CODE, INVALID_TOKEN
+
+
+item_index = randint(0, 1000)
 
 
 class TestUserItemByIndex(ApiTestBase):
@@ -14,13 +18,11 @@ class TestUserItemByIndex(ApiTestBase):
 
     def test_get_user_item_by_admin(self):
         """get user item with admin token"""
-        admintoken = self.login(DefaultUser.user, DefaultUser.password).json()["content"]
-        counter = 0
         for user in dict.keys(InitUsers.users):
-            get_item_user_response = self.get_user_item_by_index(counter, user, admintoken)
-            counter = counter + 1
+            admintoken = self.login(DefaultUser.user, DefaultUser.password).json()["content"]
+            get_item_user_response = self.get_user_item_by_index(item_index, user, admintoken)
             self.assertEqual(VALID_STATUS_CODE, get_item_user_response.status_code)
-            self.assertEqual("", get_item_user_response.json()["content"])
+            self.assertFalse(get_item_user_response.json()["content"])
 
     def test_get_user_item_by_user(self):
         """get user item with user token"""
@@ -29,5 +31,13 @@ class TestUserItemByIndex(ApiTestBase):
             token = self.login(user, password).json()["content"]
             get_item_user_response = self.get_user_item_by_index(counter, user, token)
             counter = counter + 1
+            self.assertEqual(VALID_STATUS_CODE, get_item_user_response.status_code)
+            self.assertFalse(get_item_user_response.json()["content"])
+
+    def test_get_user_item_by_invalid_token(self):
+        """get user item with invalid token"""
+        for user in dict.keys(InitUsers.users):
+            self.login(DefaultUser.user, DefaultUser.password)
+            get_item_user_response = self.get_user_item_by_index(item_index, user, INVALID_TOKEN)
             self.assertEqual(VALID_STATUS_CODE, get_item_user_response.status_code)
             self.assertFalse(get_item_user_response.json()["content"])
