4d1846af90e8244438d22542f1f28551530334b5
Pavlo Bezrukov <bezrukov0205@gmail.com>
2018-11-01 23:41:01 +0200

    refactoring code

--- tests/functional/__init__.py
+++ tests/functional/__init__.py
@@ -12,10 +12,12 @@ class ApiTestBase(unittest.TestCase):
     def setUp(self):
         """Define open request session that will be executed before each test method."""
         self.application = ApiWrapper(BaseUrl.base_url)
+        self.application.reset()
 
     def tearDown(self):
         """Define close request session and reset API data
         that will be executed after each test method."""
+        self.application.reset()
         del self.application
 
 
--- tests/functional/tests_get_item_by_index.py
+++ tests/functional/tests_get_item_by_index.py
@@ -1,8 +1,9 @@
 """Functional tests for get item by index"""
 
 from random import choice, randint
-from tests.functional import ApiTestBase
+
 from tests.constants.constants import InitUsers, VALID_STATUS_CODE, ITEM_NAMES, INVALID_TOKEN
+from tests.functional import ApiTestBase
 
 
 ITEM_INDEX = randint(0, 1000)
@@ -12,20 +13,12 @@ ITEM_NAME = choice(ITEM_NAMES)
 class TestGetItemByIndex(ApiTestBase):
     """Class for tests of get item by index"""
 
-    def setUp(self):
-        super().setUp()
-        self.reset()
-
-    def tearDown(self):
-        super().tearDown()
-        self.reset()
-
     def test_get_empty_item(self):
         """Test get item by index when user has not items"""
         for user, password in InitUsers.users.items():
             with self.subTest(i=user):
-                token = self.login(user, password).json()["content"]
-                get_item_response = self.get_item(ITEM_INDEX, token)
+                token = self.application.login(user, password).json()["content"]
+                get_item_response = self.application.get_item(ITEM_INDEX, token)
                 self.assertEqual(VALID_STATUS_CODE, get_item_response.status_code)
                 self.assertFalse(get_item_response.json()["content"])
 
@@ -33,9 +26,9 @@ class TestGetItemByIndex(ApiTestBase):
         """Test get item by index when user has item"""
         for user, password in InitUsers.users.items():
             with self.subTest(i=user):
-                token = self.login(user, password).json()["content"]
-                self.add_item(ITEM_INDEX, token, ITEM_NAME)
-                get_item_response = self.get_item(ITEM_INDEX, token)
+                token = self.application.login(user, password).json()["content"]
+                self.application.add_item(ITEM_INDEX, token, ITEM_NAME)
+                get_item_response = self.application.get_item(ITEM_INDEX, token)
                 self.assertEqual(VALID_STATUS_CODE, get_item_response.status_code)
                 self.assertTrue(get_item_response.json()["content"])
                 self.assertEqual(ITEM_NAME, get_item_response.json()["content"])
@@ -44,9 +37,9 @@ class TestGetItemByIndex(ApiTestBase):
         """Test get item by index with str index"""
         for user, password in InitUsers.users.items():
             with self.subTest(i=user):
-                token = self.login(user, password).json()["content"]
-                self.add_item(ITEM_INDEX, token, ITEM_NAME)
-                get_item_response = self.get_item("call", token)
+                token = self.application.login(user, password).json()["content"]
+                self.application.add_item(ITEM_INDEX, token, ITEM_NAME)
+                get_item_response = self.application.get_item(ITEM_NAME, token)
                 self.assertNotEqual(VALID_STATUS_CODE, get_item_response.status_code)
                 self.assertIn("Bad Request", get_item_response.text)
 
@@ -54,7 +47,7 @@ class TestGetItemByIndex(ApiTestBase):
         """Test get item by index with invalid token"""
         for user, password in InitUsers.users.items():
             with self.subTest(i=user):
-                self.login(user, password)
-                get_item_response = self.get_item(ITEM_INDEX, INVALID_TOKEN)
+                self.application.login(user, password)
+                get_item_response = self.application.get_item(ITEM_INDEX, INVALID_TOKEN)
                 self.assertEqual(VALID_STATUS_CODE, get_item_response.status_code)
                 self.assertFalse(get_item_response.json()["content"])
