5cc3e7140a789e500d212ee4707a9c2420273630
ohrytsiuk <greem92@gmail.com>
2018-11-02 11:01:37 +0200

    refactoring code for new architecture replace the api functions from functional/__init__.py to the new file application/application.py

--- tests/functional/tests_admins.py
+++ tests/functional/tests_admins.py
@@ -1,30 +1,30 @@
 """Functional tests for admins"""
 
-from tests.constants.constants import Users, DefaultUser, DefaultToken
-from tests.functional import ApiTestBase, Ascertains
+from tests.constants.constants import Users, DefaultUser, DefaultToken, VALID_STATUS_CODE
+from tests.functional import ApiTestBase
 
 
-class TestAdmins(ApiTestBase, Ascertains):
+class TestAdmins(ApiTestBase):
     """Class for testing"""
 
     def test_admins(self):
         """Get all admins with admin token. If list not empty test pass."""
-        login = self.login(DefaultUser.user, DefaultUser.password)
-        self.check_status_code_200(login.status_code)
-        all_admins = self.admins(login.json().get("content"))
-        self.check_status_code_200(all_admins.status_code)
+        login = self.application.login(DefaultUser.user, DefaultUser.password)
+        self.assertEqual(VALID_STATUS_CODE, login.status_code)
+        all_admins = self.application.admins(login.json().get("content"))
+        self.assertEqual(VALID_STATUS_CODE, all_admins.status_code)
         self.assertTrue(all_admins.json().get("content"), "Content is empty")
 
     def test_admins_user(self):
         """Get all admins without admin token. If list empty test pass"""
-        login = self.login(Users.valid_user, Users.valid_password)
-        self.check_status_code_200(login.status_code)
-        all_admins = self.admins(login.json().get("content"))
-        self.check_status_code_200(all_admins.status_code)
+        login = self.application.login(Users.valid_user, Users.valid_password)
+        self.assertEqual(VALID_STATUS_CODE, login.status_code)
+        all_admins = self.application.admins(login.json().get("content"))
+        self.assertEqual(VALID_STATUS_CODE, all_admins.status_code)
         self.assertFalse(all_admins.json().get("content"), "Content is not empty")
 
     def test_admins_default_token(self):
         """Get all admins with default token. If list empty test pass"""
-        all_admins = self.admins(DefaultToken.token)
-        self.check_status_code_200(all_admins.status_code)
+        all_admins = self.application.admins(DefaultToken.token)
+        self.assertEqual(VALID_STATUS_CODE, all_admins.status_code)
         self.assertFalse(all_admins.json().get("content"), "Content is not empty")
--- tests/functional/tests_tokenlifetime.py
+++ tests/functional/tests_tokenlifetime.py
@@ -17,7 +17,7 @@ class TestTokenLifeTime(ApiTestBase):
         Get the value of token life time
         """
 
-        resp = self.get_token_life_time()
+        resp = self.application.get_token_life_time()
 
         self.assertEqual(resp.status_code, 200)
         self.assertTrue(resp.json()["content"] or resp.json()["content"] == 0)
@@ -27,14 +27,14 @@ class TestTokenLifeTime(ApiTestBase):
         Change the token life time value by admin (positive)
         """
 
-        new_tlt = get_new_value_different_func(self.get_token_life_time, 200000, 100000)
+        new_tlt = get_new_value_different_func(self.application.get_token_life_time, 200000, 100000)
 
-        login = self.login(DefaultUser.user_admin, DefaultUser.password_admin)
+        login = self.application.login(DefaultUser.user_admin, DefaultUser.password_admin)
         token = login.json()["content"]
 
-        resp = self.change_token_life_time(token, new_tlt)
+        resp = self.application.change_token_life_time(token, new_tlt)
 
-        last_resp = self.get_token_life_time()
+        last_resp = self.application.get_token_life_time()
         tlt_after = last_resp.json()["content"]
 
         self.assertEqual(resp.status_code, 200)
@@ -46,14 +46,14 @@ class TestTokenLifeTime(ApiTestBase):
         Change the token life time value by admin (negative)
         """
 
-        new_tlt = get_new_value_different_func(self.get_token_life_time, -200000, -100000)
+        new_tlt = get_new_value_different_func(self.application.get_token_life_time, -200000, -100000)
 
-        login = self.login(DefaultUser.user_admin, DefaultUser.password_admin)
+        login = self.application.login(DefaultUser.user_admin, DefaultUser.password_admin)
         token = login.json()["content"]
 
-        resp = self.change_token_life_time(token, new_tlt)
+        resp = self.application.change_token_life_time(token, new_tlt)
 
-        last_resp = self.get_token_life_time()
+        last_resp = self.application.get_token_life_time()
         tlt_after = last_resp.json()["content"]
 
         self.assertEqual(resp.status_code, 200)
@@ -65,14 +65,14 @@ class TestTokenLifeTime(ApiTestBase):
         Change the token life time value by admin (zero)
         """
 
-        new_tlt = get_new_value_different_func(self.get_token_life_time, 0, 0)
+        new_tlt = get_new_value_different_func(self.application.get_token_life_time, 0, 0)
 
-        login = self.login(DefaultUser.user_admin, DefaultUser.password_admin)
+        login = self.application.login(DefaultUser.user_admin, DefaultUser.password_admin)
         token = login.json()["content"]
 
-        resp = self.change_token_life_time(token, new_tlt)
+        resp = self.application.change_token_life_time(token, new_tlt)
 
-        last_resp = self.get_token_life_time()
+        last_resp = self.application.get_token_life_time()
         tlt_after = last_resp.json()["content"]
 
         self.assertEqual(resp.status_code, 200)
@@ -87,12 +87,12 @@ class TestTokenLifeTime(ApiTestBase):
         new_tlt = None
         def_tlt = 1000
 
-        login = self.login(DefaultUser.user_admin, DefaultUser.password_admin)
+        login = self.application.login(DefaultUser.user_admin, DefaultUser.password_admin)
         token = login.json()["content"]
 
-        resp = self.change_token_life_time(token, new_tlt)
+        resp = self.application.change_token_life_time(token, new_tlt)
 
-        last_resp = self.get_token_life_time()
+        last_resp = self.application.get_token_life_time()
         tlt_after = last_resp.json()["content"]
 
         self.assertEqual(resp.status_code, 200)
@@ -104,17 +104,17 @@ class TestTokenLifeTime(ApiTestBase):
         Change the token life time value by admin (float)
         """
 
-        new_tlt = get_new_value_different_func(self.get_token_life_time, 200000.555, 100000)
+        new_tlt = get_new_value_different_func(self.application.get_token_life_time, 200000.555, 100000)
 
-        resp = self.get_token_life_time()
+        resp = self.application.get_token_life_time()
         curr_tlt = resp.json()["content"]
 
-        login = self.login(DefaultUser.user_admin, DefaultUser.password_admin)
+        login = self.application.login(DefaultUser.user_admin, DefaultUser.password_admin)
         token = login.json()["content"]
 
-        resp = self.change_token_life_time(token, new_tlt)
+        resp = self.application.change_token_life_time(token, new_tlt)
 
-        last_resp = self.get_token_life_time()
+        last_resp = self.application.get_token_life_time()
         tlt_after = last_resp.json()["content"]
 
         self.assertEqual(resp.status_code, 400)
@@ -127,15 +127,15 @@ class TestTokenLifeTime(ApiTestBase):
 
         new_tlt = "f%kdm525!("
 
-        resp = self.get_token_life_time()
+        resp = self.application.get_token_life_time()
         curr_tlt = resp.json()["content"]
 
-        login = self.login(DefaultUser.user_admin, DefaultUser.password_admin)
+        login = self.application.login(DefaultUser.user_admin, DefaultUser.password_admin)
         token = login.json()["content"]
 
-        resp = self.change_token_life_time(token, new_tlt)
+        resp = self.application.change_token_life_time(token, new_tlt)
 
-        last_resp = self.get_token_life_time()
+        last_resp = self.application.get_token_life_time()
         tlt_after = last_resp.json()["content"]
 
         self.assertEqual(resp.status_code, 400)
@@ -146,17 +146,17 @@ class TestTokenLifeTime(ApiTestBase):
         Change the token life time value by user (without admin rights)
         """
 
-        new_tlt = get_new_value_different_func(self.get_token_life_time, 500000, 100000)
+        new_tlt = get_new_value_different_func(self.application.get_token_life_time, 500000, 100000)
 
-        resp = self.get_token_life_time()
+        resp = self.application.get_token_life_time()
         curr_tlt = resp.json()["content"]
 
-        login = self.login(DefaultUser.user_akimatc, DefaultUser.password_akimatc)
+        login = self.application.login(DefaultUser.user_akimatc, DefaultUser.password_akimatc)
         token = login.json()["content"]
 
-        resp = self.change_token_life_time(token, new_tlt)
+        resp = self.application.change_token_life_time(token, new_tlt)
 
-        last_resp = self.get_token_life_time()
+        last_resp = self.application.get_token_life_time()
         tlt_after = last_resp.json()["content"]
 
         self.assertEqual(resp.status_code, 200)
